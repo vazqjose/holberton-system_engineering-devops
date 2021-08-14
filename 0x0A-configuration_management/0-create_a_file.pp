a1ba5bdbe04eba30a70e61ae88cae05406e12692
Jose Vazquez <vazqjose@gmail.com>
2021-08-14 06:15:50 +0000

    T0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,15 +1,13 @@
 #create a file
 
-$package = 'Holberton'
-
-package {
-  $package:
-
+class createfile {
+file { '0-create_a_file.pp':
   ensure  => 'present',
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
-  content => 'I love Puppet'
+  content => 'I love Puppet',
+   }
 }
 
