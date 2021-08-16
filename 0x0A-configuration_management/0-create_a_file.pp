767f0a4d03bdb84266b768e750c3955d04c8d64a
Jose Vazquez <vazqjose@gmail.com>
2021-08-16 20:08:15 +0000

    t0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,8 @@
 #create a file
 
-file { '/tmp/holberton/holberton':
+file { 'holberton':
   ensure  => 'present',
+  path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
