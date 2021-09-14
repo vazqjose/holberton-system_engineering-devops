0345f532a59f663626fe7d7a272109a1667b8710
Jose Vazquez <vazqjose@gmail.com>
2021-09-14 03:17:59 +0000

    t1

---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+'''
+    Using what you did in the task #0, extend your Python
+    script to export data in the CSV format.
+'''
+
+if __name__ == "__main__":
+
+    import requests
+    import sys
+    import csv
+
+    employeeID = sys.argv[1]
+
+    myData = requests.get('https://jsonplaceholder.typicode.com/users/' +
+                          employeeID)
+    name = myData.json()['username']
+
+    myTasks = requests.get(
+              'https://jsonplaceholder.typicode.com/todos?userId=' +
+              employeeID)
+    myTasks = myTasks.json()
+
+    with open("{}.csv".format(employeeID), 'w') as csvfile:
+        for val in myTasks:
+            csvfile.write(
+                    '"{}", "{}", "{}", "{}"\n'.format(
+                        employeeID, name, val['completed'], val['title']))
