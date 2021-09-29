8ae277628a30ed06966b652ef895610dd7f69ce7
Jose Vazquez <vazqjose@gmail.com>
2021-09-29 16:27:52 +0000

    completed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,6 +10,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
+    """
+    header = 'Mozilla/5.0 (X11; Linux x86_64) ' \
+             'AppleWebKit/537.36 (KHTML, like Gecko) ' \
+             'Chrome/80.0.3987.87 Safari/537.36'
+    """
     headers = {'user-agent': 'my-app/0.0.1'}
 
     response = requests.get(url, allow_redirects=False, headers=headers)
@@ -17,4 +22,4 @@ def number_of_subscribers(subreddit):
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
-        return
+        return 0
