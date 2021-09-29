c2a7fa257765ef009159f7cc50ae2e7afeddded9
Jose Vazquez <vazqjose@gmail.com>
2021-09-29 22:01:50 +0000

    task1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,5 +1,27 @@
 #!/usr/bin/python3
+'''
+    Function that queries the Reddit API and
+    prints the titles of the first 10 hot posts
+    for a given subreddit.
+'''
+
+
+import requests
 
 
 def top_ten(subreddit):
-    print ('None')
+
+    url = 'https://www.reddit.com/r/' + subreddit + '/top.json?limit=10'
+    headers = {'user-agent': 'my-app/0.0.1'}
+
+    response = requests.get(url, allow_redirects=False, headers=headers)
+
+    titles = []
+    if response.status_code == 200:
+
+        for post in response.json().get('data').get('children'):
+            line = post.get('data').get('title')
+            print(line)
+
+    else:
+        print ('None')
