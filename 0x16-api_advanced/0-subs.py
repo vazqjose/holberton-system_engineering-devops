d6e5dff78eff3b00b1820cbb25492a5aa57af329
Jose Vazquez <vazqjose@gmail.com>
2021-09-29 16:05:55 +0000

    completed

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -3,17 +3,18 @@
     query the Reddit API and returns number of subscribers for
     given subreddit
 '''
+
+
 import requests
+
 
 def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/' + subreddit + '/about.json'
-    header = 'Mozilla/5.0 (X11; Linux x86_64) ' \
-              'AppleWebKit/537.36 (KHTML, like Gecko) ' \
-              'Chrome/80.0.3987.87 Safari/537.36'
+    headers = {'user-agent': 'my-app/0.0.1'}
 
-    response = requests.get(url, allow_redirects=False, headers={'User-Agent': header})
+    response = requests.get(url, allow_redirects=False, headers=headers)
 
     if response.status_code == 200:
         return response.json().get('data').get('subscribers')
     else:
-        return 0
+        return
