91930e26f9456264457568f5f3daec1e3f0ff9cc
Jose Vazquez <vazqjose@gmail.com>
2021-09-30 14:39:07 +0000

    t1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,19 +11,18 @@ import requests
 
 def top_ten(subreddit):
 
-    url = 'https://www.reddit.com/r/' + subreddit + '/top/.json?limit=10'
-    headers = {'user-agent': 'my-app/0.0.1'}
-
+    url = 'https://www.reddit.com/r/' + subreddit + '/top.json?limit=10'
+    #headers = {'user-agent': 'my-app/0.0.1'}
+    headers = {'User-Agent': "Mozilla/5.0 (X11; Linux x86_64)\
+               AppleWebKit/537.36 (KHTML, like Gecko) Chrome/94.0.4606.61\
+               Safari/537.36"}
     response = requests.get(url, allow_redirects=False, headers=headers)
 
     if response.status_code == 200:
-        count = 10
+
         for post in response.json().get('data').get('children'):
             line = post.get('data').get('title')
             print(line)
-            count = count - 1
-            if count == 0:
-                break
 
     else:
         print('None')
