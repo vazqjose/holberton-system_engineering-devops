a3de7ac815c6111f158d5560a904d09ccb97ba69
Jose Vazquez <vazqjose@gmail.com>
2021-09-30 15:44:05 +0000

    t1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -11,7 +11,7 @@ import requests
 
 def top_ten(subreddit):
 
-    url = 'https://www.reddit.com/r/' + subreddit + '/top.json?limit=10'
+    url = 'https://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
     headers = {'user-agent': 'my-app/0.0.1'}
     response = requests.get(url, headers=headers)
 
